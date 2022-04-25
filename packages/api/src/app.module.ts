bc7f7eef8663b622ad89034b502ba6f117d392d2
Julien Prugne <julien@webeleon.dev>
2022-04-26 00:55:56 +0200

    feat(api): install and configure typeorm

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -1,13 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigType } from '@nestjs/config';
 import { appConfig } from '../configuration/app.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { databaseConfig } from '../configuration/database.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [appConfig],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule.forFeature(databaseConfig)],
+      inject: [databaseConfig.KEY],
+      useFactory: (config: ConfigType<typeof databaseConfig>) => ({
+        ...config,
+        type: 'postgres',
+        autoLoadEntities: true,
+        synchronize: true, // TODO: remove before going to production and write migrations
+      }),
     }),
   ],
   controllers: [AppController],
