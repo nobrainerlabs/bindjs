f0df3a3d16a5ff22b3877493f03d680cc1caba33
Julien Prugne <julien@webeleon.dev>
2022-04-26 00:05:38 +0200

    feat(api): add global validation pipe

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as pkg from '../package.json';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -16,6 +17,16 @@ async function bootstrap() {
     swaggerDocumentConfig,
   );
   SwaggerModule.setup('swagger', app, swaggerDocument);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
 
   await app.listen(3000);
 }
