914085e8213e2a7e0e9acd19edce6798875d6574
Julien Prugne <julien@webeleon.dev>
2022-04-25 23:52:30 +0200

    feat(api): setup swagger automatic generation

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as pkg from '../package.json';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerDocumentConfig = new DocumentBuilder()
+    .setTitle(pkg.name)
+    .setDescription(pkg.description)
+    .setVersion(pkg.version)
+    .build();
+  const swaggerDocument = SwaggerModule.createDocument(
+    app,
+    swaggerDocumentConfig,
+  );
+  SwaggerModule.setup('swagger', app, swaggerDocument);
+
   await app.listen(3000);
 }
 bootstrap();
